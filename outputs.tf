b013d8c7f6795156ef12ad2404c36a5f3c19e5cb
Daniel Allen <dan@aeternagroup.net>
2026-07-12 14:19:10 +0100

    Update to schema version v4.80.0

--- outputs.tf
+++ outputs.tf
@@ -1,37 +1,33 @@
-output "nat_gateways" {
-  description = "All nat_gateway resources"
-  value       = azurerm_nat_gateway.nat_gateways
-}
 output "nat_gateways_idle_timeout_in_minutes" {
-  description = "List of idle_timeout_in_minutes values across all nat_gateways"
-  value       = [for k, v in azurerm_nat_gateway.nat_gateways : v.idle_timeout_in_minutes]
+  description = "Map of idle_timeout_in_minutes values across all nat_gateways, keyed the same as var.nat_gateways"
+  value       = { for k, v in azurerm_nat_gateway.nat_gateways : k => v.idle_timeout_in_minutes }
 }
 output "nat_gateways_location" {
-  description = "List of location values across all nat_gateways"
-  value       = [for k, v in azurerm_nat_gateway.nat_gateways : v.location]
+  description = "Map of location values across all nat_gateways, keyed the same as var.nat_gateways"
+  value       = { for k, v in azurerm_nat_gateway.nat_gateways : k => v.location }
 }
 output "nat_gateways_name" {
-  description = "List of name values across all nat_gateways"
-  value       = [for k, v in azurerm_nat_gateway.nat_gateways : v.name]
+  description = "Map of name values across all nat_gateways, keyed the same as var.nat_gateways"
+  value       = { for k, v in azurerm_nat_gateway.nat_gateways : k => v.name }
 }
 output "nat_gateways_resource_group_name" {
-  description = "List of resource_group_name values across all nat_gateways"
-  value       = [for k, v in azurerm_nat_gateway.nat_gateways : v.resource_group_name]
+  description = "Map of resource_group_name values across all nat_gateways, keyed the same as var.nat_gateways"
+  value       = { for k, v in azurerm_nat_gateway.nat_gateways : k => v.resource_group_name }
 }
 output "nat_gateways_resource_guid" {
-  description = "List of resource_guid values across all nat_gateways"
-  value       = [for k, v in azurerm_nat_gateway.nat_gateways : v.resource_guid]
+  description = "Map of resource_guid values across all nat_gateways, keyed the same as var.nat_gateways"
+  value       = { for k, v in azurerm_nat_gateway.nat_gateways : k => v.resource_guid }
 }
 output "nat_gateways_sku_name" {
-  description = "List of sku_name values across all nat_gateways"
-  value       = [for k, v in azurerm_nat_gateway.nat_gateways : v.sku_name]
+  description = "Map of sku_name values across all nat_gateways, keyed the same as var.nat_gateways"
+  value       = { for k, v in azurerm_nat_gateway.nat_gateways : k => v.sku_name }
 }
 output "nat_gateways_tags" {
-  description = "List of tags values across all nat_gateways"
-  value       = [for k, v in azurerm_nat_gateway.nat_gateways : v.tags]
+  description = "Map of tags values across all nat_gateways, keyed the same as var.nat_gateways"
+  value       = { for k, v in azurerm_nat_gateway.nat_gateways : k => v.tags }
 }
 output "nat_gateways_zones" {
-  description = "List of zones values across all nat_gateways"
-  value       = [for k, v in azurerm_nat_gateway.nat_gateways : v.zones]
+  description = "Map of zones values across all nat_gateways, keyed the same as var.nat_gateways"
+  value       = { for k, v in azurerm_nat_gateway.nat_gateways : k => v.zones }
 }
 
